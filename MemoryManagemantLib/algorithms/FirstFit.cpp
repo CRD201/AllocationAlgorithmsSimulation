40954426cc3da60cb45088bab31f7f8bfc6bf43d
Jakub Koch <jakub.koch2001@gmail.com>
2023-12-03 13:15:28 +0100

    Debug FirstFit
    Repair FirstFir algorithm and add logs.

--- MemoryManagemantLib/algorithms/FirstFit.cpp
+++ MemoryManagemantLib/algorithms/FirstFit.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "FirstFit.hpp"
 
 namespace MML::algorithms
@@ -9,21 +10,24 @@ namespace MML::algorithms
 
     std::optional<unsigned> FirstFit::findSpace(const unsigned size)
     {
+        std::cout << "[MML-INF] FirstFit: lookoing for hole with size: " << size << std::endl;
         mmStructure.fromStart();
         unsigned holeSize{0};
         unsigned holeAdress{0};
         
-        while (! holeSize >= size)
+        while (! (holeSize >= size))
         {
             const auto hole{mmStructure.getHole()};
             if (!hole.has_value())
             {
+                std::cout << "[MML-ERR] FirstFit: no hole" << std::endl;
                 return std::nullopt;
             }
             holeSize = hole->getSize();
             holeAdress = hole->getAdress();
         }
 
+        std::cout << "[MML-INF] FirstFit: hole found: size: " << holeSize << " adress: " << holeAdress << std::endl;
         mmStructure.fillHole(size);
         return std::make_optional<unsigned>(holeAdress);
     }
